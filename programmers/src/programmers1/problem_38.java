ee2f4bbd6a062cde9c6f91cf8a92c88d0cb9ab1c
jms8732 <jms8732@dongguk.edu>
2020-01-14 16:15:13 +0900

    서울에서 김서방 찾기
    programmers

--- programmers/src/programmers1/problem_38.java
+++ programmers/src/programmers1/problem_38.java
@@ -1,5 +1,18 @@
 package programmers1;
 
-public class problem_38 {
+//서울에서 김서방 찾기
 
+public class problem_38 {
+	public static String solution(String[] seoul) {
+		int answer =0 ;
+		for(int i = 0; i < seoul.length ; i++) {
+			if(seoul[i].equals("Kim")) {
+				answer =i ;
+				break;
+			}
+		}
+		
+		
+		return "김서방은 " + answer +"에 있다";
+	}
 }
