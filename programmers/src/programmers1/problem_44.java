ec04f5fc1697d101170aa6ff4df69257d4862d24
jms8732 <jms8732@dongguk.edu>
2020-01-15 17:17:24 +0900

    수박수박수박수박수박수?
    programmers

--- programmers/src/programmers1/problem_44.java
+++ programmers/src/programmers1/problem_44.java
@@ -1,5 +1,14 @@
 package programmers1;
 
+//수박수박수박수박수박수?
 public class problem_44 {
-
+	public static String solution(int n) {
+		char[] array = {'수','박'};
+		StringBuilder sb= new StringBuilder();
+		for(int i =0 ; i< n ; i++) {
+			sb.append(array[i%array.length]);
+		}
+		
+		return sb.toString();
+	}
 }
