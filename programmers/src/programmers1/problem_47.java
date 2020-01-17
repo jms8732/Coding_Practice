bba3ce82ca5190fb8d002f2dfde2f05b49845dc4
jms8732 <jms8732@dongguk.edu>
2020-01-17 16:45:01 +0900

    약수의 합
    programmers

---
+++ programmers/src/programmers1/problem_47.java
@@ -0,0 +1,19 @@
+package programmers1;
+
+//약수의 합
+public class problem_47 {
+	public static void main(String[] args) {
+		int n = 12;
+		int result = solution(n);
+		System.out.println(result);
+	}
+	public static int solution(int n) {
+		int answer =0 ;
+		for(int i = n ; i>= 1; i--) {
+			if(n % i == 0)
+				answer +=i;
+		}
+		
+		return answer;
+	}
+}
