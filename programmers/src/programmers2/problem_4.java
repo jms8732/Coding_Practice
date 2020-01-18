9e2a1e3e5659939e88183bf7f816930aaba7ca9c
jms8732 <jms8732@dongguk.edu>
2020-01-18 14:58:10 +0900

    짝수와 홀수
    prorgrammers

--- programmers/src/programmers2/problem_4.java
+++ programmers/src/programmers2/problem_4.java
@@ -1,5 +1,12 @@
 package programmers2;
 
+//礎熱諦 �汝�
 public class problem_4 {
-
+	public String solution(int num) {
+		if(num % 2 == 0) {
+			//礎熱檣 唳辦
+			return "Even";
+		}else
+			return "Odd";
+	}
 }
