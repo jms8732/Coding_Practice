f4ed69cc9a08e77d58b3a4db37a2c07ba962e404
jms8732 <jms8732@dongguk.edu>
2020-01-25 15:42:28 +0900

    콜라츠 추측
    programmers

---
+++ programmers/src/programmers2/problem_8.java
@@ -0,0 +1,36 @@
+package programmers2;
+
+//ÄÝ¶óÃ÷ ÃßÃø
+
+public class problem_8 {
+	public static void main(String[] args) {
+		int n = 626331;
+		System.out.println(solution(n));
+	}
+	public static int solution(int num) {
+		int round = 0;
+		long i= num;
+		while(true) {
+			if(i == 1)
+			{
+				break;
+			}
+			
+			if(round == 500)
+			{
+				round = -1;
+				break;
+			}
+			
+			if(i % 2 == 0) {
+				//Â¦¼öÀÌ¸é
+				i /= 2;
+			}else
+				i = 3*i + 1;
+			round++;
+		}
+		
+		
+		return round;
+	}
+}
