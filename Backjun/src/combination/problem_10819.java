d4272a89e3a1c3a76241ed56c8aedd329eab82bc
jms8732 <jms8732@dongguk.edu>
2020-02-27 15:00:29 +0900

    10819. 차이를 최대로
    baekjoon

---
+++ Backjun/src/combination/problem_10819.java
@@ -0,0 +1,55 @@
+package combination;
+
+import java.util.*;
+import java.io.*;
+
+//차이를 최대로
+public class problem_10819 {
+	static int big = 0;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int [] array= new int[N];
+		
+		for(int i =0 ; i < N ; i++) {
+			array[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int depth = 0;
+		int[] answer= new int[N];
+		permutation(depth,array,answer);
+		System.out.println(big);
+	}
+	private static void permutation(int depth, int[] array, int [] answer) {
+		if(depth == array.length) {
+			int tmp =0;
+			for(int i =0 ; i< answer.length ; i++) {
+				int cur = i;
+				int next = i+1;
+				if(cur < answer.length && next < answer.length) {
+					//범위내에 존재할 경우
+					tmp += Math.abs(answer[cur] - answer[next]);
+				}
+			}
+			
+			big = Math.max(big, tmp);
+			
+			return;
+		}
+		
+		for(int i = depth ; i < array.length; i ++) {
+			answer[depth] = array[i];
+			swap(depth,i,array);
+			permutation(depth+1,array,answer);
+			swap(i,depth,array);
+		}
+	}
+	private static void swap(int depth, int i, int[] array) {
+		int tmp = array[i];
+		array[i] = array[depth];
+		array[depth] = tmp;
+	}
+}
