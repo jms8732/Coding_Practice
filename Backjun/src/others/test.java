73bd923dabd56f5af7924d047a28aa24d2b5e1b4
jms8732 <jms8732@dongguk.edu>
2020-03-25 22:04:25 +0900

    [2020 카카오공채] 외벽점검
    programmers

--- Backjun/src/others/test.java
+++ Backjun/src/others/test.java
@@ -2,9 +2,12 @@ package others;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println(10000);
-		for(int i =0 ; i < 10000 ; i++) {
-			System.out.println(1000000);
+		for(int i =1 ; i < (1<<4); i++) {
+			for(int j= 0 ; j < 4; j ++) {
+				if((i & 1<<j ) == 1<<j)
+					System.out.print(j + " ");
+			}
+			System.out.println();
 		}
 	}
 }
