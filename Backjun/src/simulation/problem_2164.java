0fbee680f440695026d893f1f76cbbe7500f7de9
jms8732 <jms8732@dongguk.edu>
2020-05-15 15:15:36 +0900

    2164. 카드2
    baekjoon

---
+++ Backjun/src/simulation/problem_2164.java
@@ -0,0 +1,28 @@
+package simulation;
+
+//카드 2
+import java.util.*;
+import java.io.*;
+
+public class problem_2164 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		
+		Queue<Integer> q = new LinkedList<>();
+		
+		for(int i =0 ; i < N ; i++) {
+			q.add(i+1);
+		}
+		
+		while(q.size() != 1) {
+			q.poll(); //맨 위의 카드를 버린다.
+			
+			int top = q.poll();
+			q.add(top);
+		}
+		
+		System.out.println(q.poll());
+	}
+}
